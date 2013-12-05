9eed5ea14c2bb522bfd7e7c5a89a9be936dbe070
Daniel Oertwig <Daniel.Oertwig@gmail.com>
2013-12-05 02:50:35 +0100

    Draft of next parsing phase (token -> entities)

--- ParserDraft.java
+++ ParserDraft.java
@@ -3,20 +3,120 @@ import java.nio.charset.StandardCharsets;
 
 public class ParserDraft
 {
-	public static class Token
+	public static void t()
 	{
-		protected int lineNumber;
-		protected int start;
-		protected int end;
-		// protected CharSequence underlyingData;
+		while (tokenizer.hasToken())
+		{
+			Token token = tokenizer.nextToken();
+			if (isLabel(token))
+			{
+				// register label with value
+				// if next token is instruction token, register number
+				// else register token itself
+			}
+			else if (isInstruction(token))
+			{
+				// Must be an instruction token
+				// parse instruction tokens parameters
+			}
+			else
+			{
+				// Error
+			}
+		}
 	}
 
-	public static void fromFile(String filename)
+	public static void p()
 	{
-		FileInputStream fis = new FileInputStream(filename);
-		FileChannel fc = fis.getChannel();
-		ByteBuffer byteBuffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-		CharBuffer charBuffer = StandardCharsets.UTF_8.decode(byteBuffer);
+		int instructionCounter = 0;
+		int instructionParameters = 0;
+		Token labelToRegister = null;
+		while (tokenizer.hasToken())
+		{
+			Token token = tokenzier.nextToken();
+			if (isLabel(token))
+			{
+				if (labelToRegister == null)
+				{
+					labelToRegister = token;
+					continue;
+				}
+				else
+				{
+					throw new ParseException();
+				}
+			}
+			if (isInstruction(token))
+			{
+				// get instruction
+				// read instruction Parameter count
+			}
+		}
+	}
+
+	public static class InstructionEntity implements ParseEntity
+	{
+		protected Class instructionClass;
+		protected int instructionAddress;
+		protected int lineNumber;
+		public InstructionEntity(int address, Class instructionClass, int line)
+		{
+			this.instructionAddress = address;
+			this.instructionClass = instructionClass;
+			this.lineNumber = line;
+		}
+		public int getValue()
+		{
+			return this.instructionAddress;
+		}
+	}
+
+
+	public static void v()
+	{
+		int instructionCounter = 0;
+		List<ParseEntity> parseEntities = new LinkedList<ParseEntity>();
+		while (tokenizer.hasToken())
+		{
+			Token token = tokenizer.nextToken();
+			if (instructionMap.containsKey(token))
+			{
+				InstructionEntity instructionEntity =
+					new InstructionEntity(
+							instructionCounter,
+							instructionMap.get(token),
+							token.getLineNumber());
+				parseEntities.addLast(instructionEntity);
+			}
+			else if (token.endsWith(":"))
+			{
+				LabelDeclarationEntity lde =
+					new LabelDeclarationEntity(token);
+				parseEntities.addLast(lde);
+			}
+			else
+			{
+				try
+				{
+					int value = Integer.decode(token);
+					NumberEntity numberEntity =	new NumberEntity(value);
+					parseEntities.addLast(numberEntity);
+				}
+				catch (NumberFormatException e)
+				{
+					// Not a number, must be a label
+					LabelReferenceEntity lre =
+						new LabelReferenceEntity(token);
+					parseEntities.addLast(lre);
+				}
+			}
+		}
+	}
+
+
+	public static interface ParseEntity
+	{
+		public abstract int getValue();
 	}
 }
 
