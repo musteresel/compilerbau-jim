49c6df0e16991a5fda6b95e04096eec155d7061c
Daniel Oertwig <Daniel.Oertwig@gmail.com>
2013-12-06 03:01:58 +0100

    Commented parse implementation.

--- parser/ParserUnit.java
+++ parser/ParserUnit.java
@@ -13,15 +13,21 @@ public class ParserUnit
 		this.instructions = new LinkedList<InstructionStub>();
 		this.referenceMap = new HashMap<Token, Type>();
 		int instructionCounter = 0;
+		// Token to be used in the next mapping.
 		Token nextMapping = null;
+		// Set to the last added instruction, or null, if last processed token
+		// was not an instruction or part thereof.
 		InstructionStub lastInstructionStub = null;
 		while (tokenizer.hasToken())
 		{
 			Token token = tokenizer.nextToken();
+			// Check whether the token is an instruction.
 			if (instructionMap.containsKey(token))
 			{
 				lastInstructionStub =
 					new InstructionStub(instructionMap.get(token));
+				// If the previous token was a label declaration, map this
+				// instructions address to the label.
 				if (nextMapping != null)
 				{
 					this,referenceMap.put(nextMapping,
@@ -31,8 +37,11 @@ public class ParserUnit
 				instructionCounter++;
 				this.instructions.addLast(lastInstructionStub);
 			}
+			// Check whether token is a label declaration.
 			else if (token.endsWith(":"))
 			{
+				// In case the previous token was a label declaration, there
+				// is a parse failure.
 				if (nextMapping != null)
 				{
 					this.log_failure(new ParseFailure("Label declaration of " +
@@ -40,13 +49,19 @@ public class ParserUnit
 								") after declaration of " + nextMapping + " (Line " +
 								Integer.toString(nextMapping.getLineNumber()) + ")."));
 				}
+				// Try to proceed in order to evaluate as much as possible, and thus
+				// providing detailed error reporting.
 				nextMapping = token;
+				// A label declaration "ends" the parameter list of a previous
+				// instruction.
 				lastInstructionStub = null;
 			}
+			// Neither instruction nor declaration, must be value or reference.
 			else
 			{
 				ValueStub valueStub = null;
 				Type type = null;
+				// Check if token can be converted to any known value type.
 				IntegerType iType = new IntegerType();
 				DoubleType dType = new DoubleType();
 				if (iType.from(token))
@@ -57,21 +72,29 @@ public class ParserUnit
 				{
 					type = dType;
 				}
+				// If token could be converted, use the converted type instance.
 				if (type != null)
 				{
+					// If the previous token was a label declaration, map this
+					// type to the label.
 					if (nextMapping != null)
 					{
 						this.referenceMap.put(nextMapping, type);
 						nextMapping = null;
 					}
+					// The type is probably used as a parameter to an instruction.
 					else
 					{
 						valueStub = new EvaluatedValueStub(type);
 					}
 
 				}
+				// In case the token could not be converted, it must be a reference
+				// to a somewhere defined label.
 				else
 				{
+					// If the previous token was a label declaration, fail parsing
+					// because we do not support recursive label references.
 					if (nextMapping != null)
 					{
 						this.log_failure(new ParseFailure("Label declaration of " +
@@ -80,17 +103,21 @@ public class ParserUnit
 									") tries to use reference " + token "."));
 						nextMapping = null;
 					}
+					// The reference is probably a parameter to an instruction.
 					else
 					{
 						valueStub = new ReferencedValueStub(token);
 					}
 				}
+				// The token is not used up to this point.
 				if (valueStub != null)
 				{
+					// Try to use it as parameter to the last read instruction.
 					if (lastInstructionStub != null)
 					{
 						lastInstructionStub.push_parameter(valueStub);
 					}
+					// Token is not used anywhere, this is a failure.
 					else
 					{
 						this.log_failure(new ParseFailure("Orphan token at line " +
@@ -99,6 +126,7 @@ public class ParserUnit
 					}
 				}
 			}
+			// Continue with next token.
 		}
 		this.failures = new LinkedList<ParseFailure>();
 	}
